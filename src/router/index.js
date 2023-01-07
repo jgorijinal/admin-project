42fa9f4ab3a5f56f335f5ce8e06dc34b438c1166
jgoriginal <932274204@qq.com>
2023-01-07 17:06:39 +0800

    创建登录页面和路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
-const routes = [
+// 公共路由表: 所有用户都等访问的页面
+const publicRoute = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+    path: '/login',
+    component:()=> import('@/views/login/index.vue')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes:publicRoute
 });
 
 export default router;
