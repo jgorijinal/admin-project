237d4421fd0048f5aef909bad8e0577215f4a031
jgoriginal <932274204@qq.com>
2023-01-08 00:43:50 +0800

    svg-sprite-loader 处理 svg 图标

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './styles/index.scss'
+import installIcons from '@/icons'
 // 全局注册 Element Plus 的图标
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+// 导入 svgIcon
+installIcons(app)
 
 app.use(store).use(router).mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,10 @@ const { defineConfig } = require("@vue/cli-service");
 const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
-
+const path = require('path')
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
@@ -15,5 +18,23 @@ module.exports = defineConfig({
         resolvers: [ElementPlusResolver()],
       }),
     ]
+  },
+  chainWebpack(config) {
+    // 设置 svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
   }
 });
