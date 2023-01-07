01c483c2267d37bb1b1c61dc7ccbc5ddd3e913b4
jgoriginal <932274204@qq.com>
2023-01-07 20:30:16 +0800

    按需引入 Elemnt Plus并配置vue.config.js

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    login
+    <el-form
+      ref="ruleFormRef"
+      :model="formData"
+      :rules="rules"
+      label-width="120px"
+  >
+    <el-form-item label="用户名" prop="username">
+      <el-input v-model="formData.username" type="text" autocomplete="off" />
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input v-model="formData.password" type="password" autocomplete="off" />
+    </el-form-item>
+  </el-form>
   </div>
 </template>
 <script setup>
+import { ref } from 'vue'
+// 表单数据
+const formData = ref({
+  username: 'cuiyang123',
+  password: ''
+})
+
 </script>
 <style lang="scss" scoped>
   
