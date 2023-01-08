e892144dc125bdbdfb27d7eb3bd7224d324d2539
jgoriginal <932274204@qq.com>
2023-01-08 13:03:59 +0800

    实现登录鉴权

--- src/permission.js
+++ src/permission.js
@@ -1,5 +1,21 @@
 import router from './router';
+import store from '@/store'
 
+const whiteList = ['/login']
 router.beforeEach((to, from, next) => {
-  
+  if (store.getters.token) {
+    // 已登录
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 未登录
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login') 
+    }
+  }
 });
